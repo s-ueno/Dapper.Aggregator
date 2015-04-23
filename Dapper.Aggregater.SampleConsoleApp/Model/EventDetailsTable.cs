259a87a23af4146a753a063287cc6187f0cf79b5
Shinichi Ueno <uen.agile@gmail.com>
2015-04-23 23:30:24 +0900

    漏れ

---
+++ Dapper.Aggregater.SampleConsoleApp/Model/EventDetailsTable.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper.Aggregater.SampleConsoleApp.Model
+{
+    [Table("EventDetailsTable")]
+    public class EventDetailsTable : IContainerHolder
+    {
+        public int EventTableID { get; set; }
+        public int EventDetailsTableID { get; set; }
+        public int CodeTableID { get; set; }
+        public byte[] Lockversion { get; set; }
+
+        DataContainer IContainerHolder.Container { get; set; }
+    }
+}
